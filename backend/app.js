e5d6206e62cab4a88e69dba1b70c06ac8acf0e2d
RobertRuai <robertruai4@gmail.com>
2024-04-15 14:53:50 +0300

    added mongodb connection function

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
+const host = 'localhost';
 
+//Middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
-const PORT = process.env.PORT || 3000;
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/tax-payment-system', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err));
+
+// Routes
+const authRoutes = require('./routes/authRoutes');
+const paymentRoutes = require('./routes/paymentRoutes');
+app.use('/api/auth', authRoutes);
+app.use('/api/payment', paymentRoutes);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on http://${host}:${PORT}`);
 });
