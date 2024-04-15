5125e84ffd50b864fbade3d935926eb0904426ad
RobertRuai <robertruai4@gmail.com>
2024-04-15 19:36:54 +0300

    small edit on mongodb connection function

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,17 @@
+require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const host = 'localhost';
+const connectDB = require('../config/dbConn')
+
+connectDB()
 
 //Middleware
 app.use(bodyParser.json());
 app.use(cors());
-
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/tax-payment-system', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.log(err));
 
 // Routes
 const authRoutes = require('./routes/authRoutes');
@@ -24,6 +20,9 @@ app.use('/api/auth', authRoutes);
 app.use('/api/payment', paymentRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://${host}:${PORT}`);
-});
+
+// Connect to MongoDB
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server running on port http://${host}:${PORT}`))
+})
